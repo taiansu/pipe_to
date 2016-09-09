ca0b86e45dd3339286e66bb5d6c902fe2ab4577d
Tai An Su <i@taian.su>
2016-09-10 02:53:25 +0800

    Add Doc

---
+++ doc/dist/sidebar_items.js
@@ -0,0 +1,1 @@
+sidebarNodes={"exceptions":[],"extras":[{"id":"api-reference","title":"API Reference","headers":[]}],"modules":[{"id":"PipeTo","title":"PipeTo","functions":[{"id":"unpipe/1","anchor":"unpipe/1"}],"macros":[{"id":"~>/2","anchor":"~%3E/2"}]},{"id":"PipeTo.Override","title":"PipeTo.Override","macros":[{"id":"|>/2","anchor":"%7C%3E/2"}]}],"protocols":[]}
