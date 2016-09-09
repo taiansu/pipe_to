bae7e332eb1dafd966daac80a326feac1bba6a5f
Tai An Su <i@taian.su>
2016-09-10 02:53:10 +0800

    Improve mix.exs
    * Fullfill the hex package requirement
    * add ex_doc and excoveralls

--- mix.exs
+++ mix.exs
@@ -5,28 +5,37 @@ defmodule PipeTo.Mixfile do
     [app: :pipe_to,
      version: "0.1.0",
      elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+     name: "PipeTo",
+     source_url: "https://github.com/taiansu/pipe_to",
+     homepage_url: "https://taiansu.github.io/pipe_to",
+     description: description(),
+     package: package(),
+     deps: deps(),
+     test_coverage: [tool: ExCoveralls]]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp description do
+    """
+    PipeTo operator `~>` is the enhanced pipe operator which can specify the target position.
+    """
+  end
+
+  defp package do
+    [ files: ["lib", "priv", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Tai An Su"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub": "https://github.com/taiansu/pipe_to",
+               "Docs": "https://taiansu.github.io/pipe_to"}]
+  end
+
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.13", only: :dev},
+      {:excoveralls, "~> 0.5", only: [:dev, :test]}
+    ]
   end
 end
